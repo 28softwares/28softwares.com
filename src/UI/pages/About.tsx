a77ff75543d26c9127af0534ef2e9b86216592c1
kshetritej <tejghartikshetri@gmail.com>
2024-10-23 22:03:59 +0545

    patch(top-product-card,projects-card): change design

--- src/UI/pages/About.tsx
+++ src/UI/pages/About.tsx
@@ -9,10 +9,21 @@ export const About = () => {
     <Layout>
       <section className="py-12 bg-background">
         <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-8">
-            Amazing peoples who have working/worked with us
-            <span className="text-primary">.</span>
-          </h2>
+          <div className=" flex flex-col gap-2  md:justify-center items-start md:items-center text-center ">
+            <p className="font-bold text-xl text-primary">About Us</p>
+            <p className="text-center w-full py-4">
+              The named behind 28Softwares is that, we are making 28 different
+              software products for different areas (such as Hotel lines,
+              schools, Hospitals etc.). These softwares are generally MIS, ERP
+              or CRM which are powered by AI/ML. Besides our in house projects,
+              we also work for out-source/freelance projects doing Web
+              Development, Mobile App Development, and other software
+              development services.
+            </p>
+            <p className="text-left  text-lg lg:text-4xl font-bold pb-8">
+              Amazing People, working with us
+            </p>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {members.map((member, index) => (
               <Card key={index} className="overflow-hidden">
