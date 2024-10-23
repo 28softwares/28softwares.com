cf2b12568cf8e9b519e128ea71e4d083e6bfbc65
Saroj Dahal <isarojdahal@gmail.com>
2024-10-23 23:04:54 +0545

    update: 'The' to caps in Aboutus page

--- src/UI/pages/About.tsx
+++ src/UI/pages/About.tsx
@@ -10,13 +10,13 @@ export const About = () => {
           <div className=" flex flex-col gap-2  md:justify-center items-start md:items-center text-center ">
             <p className="font-bold text-xl text-primary">About Us</p>
             <p className="text-center w-full py-4">
-              The named behind 28Softwares is that, we are making 28 different
-              software products for different areas (such as Hotel lines,
-              schools, Hospitals etc.). These softwares are generally MIS, ERP
-              or CRM which are powered by AI/ML. Besides our in house projects,
-              we also work for out-source/freelance projects doing Web
-              Development, Mobile App Development, and other software
-              development services.
+              <span className="text-primary text-4xl">The</span> named behind
+              28Softwares is that, we are making 28 different software products
+              for different areas (such as Hotel lines, schools, Hospitals
+              etc.). These softwares are generally MIS, ERP or CRM which are
+              powered by AI/ML. Besides our in house projects, we also work for
+              out-source/freelance projects doing Web Development, Mobile App
+              Development, and other software development services.
             </p>
             <p className="text-left  text-lg lg:text-4xl font-bold pb-8">
               Amazing People, working with us
